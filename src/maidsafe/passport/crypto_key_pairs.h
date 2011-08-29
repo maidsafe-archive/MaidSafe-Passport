24b592489eb7a77fcf1f1d68fbdf161421ef5e0f
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2011-08-30 00:52:14 +0100

    Updated to version 0.01.06 Added ChangeLog.txt

--- src/maidsafe/passport/crypto_key_pairs.h
+++ src/maidsafe/passport/crypto_key_pairs.h
@@ -33,7 +33,7 @@
 #include "boost/thread.hpp"
 #include "maidsafe/passport/version.h"
 
-#if MAIDSAFE_PASSPORT_VERSION != 105
+#if MAIDSAFE_PASSPORT_VERSION != 106
 #  error This API is not compatible with the installed library.\
     Please update the maidsafe-passport library.
 #endif
